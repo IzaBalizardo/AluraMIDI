8cea905278f859145333610d8e5002c7202a7535
Izabela Balizardo <izah_rb@hotmail.com>
2022-10-16 17:33:44 -0300

    Finaliza projeto aluraMidi

--- main.js
+++ main.js
@@ -1,3 +1,26 @@
-document.querySelector('.tecla_pom');
+const listaDeTeclas = document.querySelectorAll('.tecla');
 
-document.querySelector('#som_tecla_pom').play();
+
+
+function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
+}
+
+let contador = 0;
+
+while (contador < listaDeTeclas.length) {
+
+    const tecla = listaDeTeclas[contador];
+   const instrumento =  tecla.classList[1];
+
+   const idAudio = `#som_${instrumento}`;
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+
+    contador = contador + 1;
+}
+
+
+
